b0b092f59acd3da3d44ccd03a483c2a06034889d
JiaCheng <xiejc2000@gmail.com>
2016-09-16 21:59:54 +0800

    Updated to included base for subscription

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ const BOT_TOKEN = process.env.BOT_TOKEN;
 
 var text = '';
 var users = [];
+var thing = {id:"", name:"", alert:""};
 var prev_time = '';
 
 console.log('Starting...');
@@ -25,6 +26,7 @@ function psi_callback(error, response, body) {
   if (!error && response.statusCode == 200) {
     var info = JSON.parse(body);
     if (prev_time != info.items[0].update_timestamp){//only update text if there's a new update
+      prev_time = info.items[0].update_timestamp;
       var readings = (info.items[0].readings.psi_three_hourly);
       text = '\n\nCurrent 3 Hour PSI Readings:\n'
         + '\nNational: ' + readings.national
@@ -71,22 +73,44 @@ console.log('Bot started');
 
 bot.onText(/\/start/, function (msg, match){
   bot.sendMessage(msg.chat.id, 'Hi! I\'m HazeBot, /update to get the latest data!');
-  var added = false;
-  for(var i = 0; i < users.length; i++){
-    if (msg.chat.id == users[i]) {
-      added = true;
-      break;
-    }
+  if (findUser(msg.chat.id)) {
+    users[users.length].id = msg.chat.id;
+    users[users.length].name = msg.chat.name;
   }
-  if (!added) {
-    users[users.length] = msg.chat.id;
+  for (var i = 0; i < users.length; i++) {
+    console.log(i + ':' + users[i].name);
   }
-  console.log(msg);
-  console.log('Total number of users:' + users.length);
-  console.log('\n');
 });
 
 bot.onText(/\/update/, function(msg) {
-  request(psi_options, psi_callback);
   bot.sendMessage(msg.chat.id, text);
 })
+
+bot.onText(/\/subscribe/, function (msg) {
+  var u = findUser(msg.chat.id);
+  if (u != -1) {
+    users[i].alert = true;
+    bot.sendMessage(msg.chat.id, "Subscribed");
+  }
+})
+
+bot.onText(/\/unsubscribe/, function (msg) {
+  var u = findUser(msg.chat.id);
+  if (u != -1) {
+    users[i].alert = false;
+    bot.sendMessage(msg.chat.id, "Unsubscribed");
+  }
+})
+
+function findUser(id) {
+  var user = -1;
+  for(var i = 0; i < users.length; i++){
+    if (id == users[i].id) {
+      user = i;
+      break;
+    }
+  }
+  return user;
+}
+
+setInterval(function() {request(psi_options, psi_callback)}, 30000);
